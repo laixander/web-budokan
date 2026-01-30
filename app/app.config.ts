4cbed861258562db1175a4cc14781263896f8215
laixander <laixander@gmail.com>
2026-01-31 00:21:29 +0800

    Updates

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,30 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'green',
+      primary: 'red',
       neutral: 'slate'
+    },
+    button: {
+      slots: {
+        base: 'font-bold uppercase tracking-widest'
+      },
+      variants: {
+        size: {
+          md: {
+            base: 'px-4 py-2.5 text-sm gap-1.5',
+            leadingIcon: 'size-5',
+            leadingAvatarSize: '2xs',
+            trailingIcon: 'size-5'
+          },
+        }
+      },
+      compoundVariants: [
+        {
+          color: 'primary',
+          variant: 'solid',
+          class: 'text-white bg-red-700 hover:bg-red-700/75 active:bg-red-700/75 disabled:bg-red-700 aria-disabled:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-700'
+        },
+      ]
     }
   }
 })
---
+++ app/pages/ui.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-screen bg-zinc-950 text-zinc-100 flex flex-col font-sans">
+        <NavbarGuest />
+        <main id="top" class="flex-grow pt-16">
+            <section id="about" class="py-24 bg-zinc-950">
+                <div class="max-w-4xl mx-auto px-4 text-center flex gap-2">
+                    <UButton>Button</UButton>
+                    <UButton icon="i-lucide-log-out">Logout</UButton>
+                </div>
+            </section>
+        </main>
+        <Footer />
+    </div>
+</template>
